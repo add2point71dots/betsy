e8def38cfcdf4eef97dce591b970c425a36b9188
Bo Trethewey <botrethewey@gmail.com>
2017-04-19 07:59:45 -0700

    added validations to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,15 @@
 class Order < ApplicationRecord
+  has_many :orderitems
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :name_on_card, presence: true
+  validates :card_number, presence: true
+  validates :exp_month, presence: true
+  validates :exp_year, presence: true
+  validates :cvv, presence: true
+  validates :billing_zip_code, presence: true
 end
