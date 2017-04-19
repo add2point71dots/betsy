12dad7428b7dbd56501273660f9a749baee06a25
add2point71dots <alcland@gmail.com>
2017-04-19 00:30:15 -0700

    created review model

---
+++ test/models/review_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+describe Review do
+  let(:review) { Review.new }
+
+  describe "validations" do
+
+  end
+
+  describe "relationships" do
+    #write relationship tests here
+  end
+end
