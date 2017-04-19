c44f992776ac6d565afd37876235cdb3ba5e9e93
Bo Trethewey <botrethewey@gmail.com>
2017-04-19 14:14:45 -0700

    update tests for orderitem

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -8,18 +8,18 @@ describe Orderitem do
       v.user.must_be_kind_of Order
     end
 
-    it "has a product" do skip
-      v = orderitems(:one)
-      v.must_respond_to :product
-      v.user.must_be_kind_of Product
-    end
+    # it "has a product" do skip
+    #   v = orderitems(:one)
+    #   v.must_respond_to :product
+    #   v.user.must_be_kind_of Product
+    # end
   end
 
   describe "validations" do
     it "allows the four order_states" do
       valid_states = ['pending', 'paid', 'completed', 'canceled']
       valid_states.each do |state|
-        orderitem = Orderitem.new(order: orders(:one), order_state: state)
+        orderitem = Orderitem.new(order_id: orders(:one).id, order_state: state)
         orderitem.valid?.must_equal true
       end
     end
@@ -27,7 +27,7 @@ describe Orderitem do
     it "rejects invalid order_state" do
       invalid_states = ['lost', 'shipped', 'backordered', 'banana']
       invalid_states.each do |invalid_state|
-        orderitem = Orderitem.new(order: orders(:one), order_state: invalid_state)
+        orderitem = Orderitem.new(order_id: orders(:one).id, order_state: invalid_state)
         orderitem.valid?.must_equal false
         orderitem.errors.messages.must_include :order_state
       end
