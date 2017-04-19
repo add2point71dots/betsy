7a668ee4bfe6fbe7f55672411afc6c22e9c9690c
janicewilson <janice.wilson@mac.com>
2017-04-19 11:44:28 -0700

    Retracted effort to make has_and_belongs_to_many relationship work.

--- db/migrate/20170419045027_create_product_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateProductCategories < ActiveRecord::Migration[5.0]
-     def change
-          create_table :products_categories, id: false do |t|
-               t.belongs_to :product, index: true
-               t.belongs_to :category, index: true
-          end
-     end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419050829) do
+ActiveRecord::Schema.define(version: 20170419044618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20170419050829) do
     t.string   "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "product_categories", id: false, force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "category_id"
-    t.index ["category_id"], name: "index_product_categories_on_category_id", using: :btree
-    t.index ["product_id"], name: "index_product_categories_on_product_id", using: :btree
   end
 
 end
