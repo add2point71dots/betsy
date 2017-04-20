1efdef8dedb8dbaffb2be389da01565a8d38ee1d
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-20 16:05:27 -0700

    Product validations test passed after updating fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -7,54 +7,48 @@ describe Product do
   describe "validations" do
 
     it "can create a product with valid name, price and quantity" do
-      # product = Product.new(name: "blender", price: 99.9, quantity: 2)
       product = products(:two)
        product.valid?.must_equal true
     end
 
-    # it "can't create a product without a valid name" do
-    #   product = Product.new(name: "", price: 99.9, quantity: 2)
-    #   product.valid?.must_equal false
-    #   product.errors.messages.must_include :name
-    #
-    # end
-    #
-    # it "product name should be unique" do
-    #   product_1= products(:blue1)
-    #   product_2 = Product.new(name: "blender", price: 99.9, quantity: 2)
-    #   product_2.valid?.must_equal false
-    #
-    # end
-    #
-    #
-    # it "can't create a product without a valid price" do
-    #   product_2 = Product.new(name: "blender", price: nil, quantity: 2)
-    #   product_2.valid?.must_equal false
-    # end
-    #
-    # it "price should be integer " do
-    #   product_2 = Product.new(name: "blender", price: "fourty five", quantity: 2)
-    #   product_2.valid?.must_equal false
-    # end
-    #
-    # it "can't create a product with price = 0" do
-    #     product_2 = Product.new(name: "blender", price: 0, quantity: 2)
-    #     product_2.valid?.must_equal false
-    #   end
-    #
-    # it "can't create a product without quantity" do
-    #   product_2 = Product.new(name: "blender", price: 14.99, quantity: nil)
-    #   product_2.valid?.must_equal false
-    # end
-    #
-    #
-    # it "quantity should be integer" do
-    #   #products(:blue4).valid?.must_equal false
-    #   product_2 = Product.new(name: "hat", price: 14.99, quantity: "four")
-    #   product_2.valid?.must_equal false
-    #   product_2.errors.messages.must_include :quantity
-    # end
+    it "can't create a product without a valid name" do
+      product = Product.new(name: "", price: 99.9, quantity: 2)
+      product.valid?.must_equal false
+      product.errors.messages.must_include :name
+    end
 
+    it "product name should be unique" do
+      product_1= products(:three)
+      product_2 = Product.new(name: "scarf", price: 99.9, quantity: 2)
+      product_2.valid?.must_equal false
+    end
+
+    it "can't create a product without a valid price" do
+      product_2 = Product.new(name: "blender", price: nil, quantity: 2)
+      product_2.valid?.must_equal false
+    end
+
+    it "price should be integer " do
+      product_2 = Product.new(name: "blender", price: "fourty five", quantity: 2)
+      product_2.valid?.must_equal false
+    end
+
+    it "can't create a product with price = 0" do
+        product_2 = Product.new(name: "blender", price: 0, quantity: 2)
+        product_2.valid?.must_equal false
+      end
+
+    it "can't create a product without quantity" do
+      product_2 = Product.new(name: "blender", price: 14.99, quantity: nil)
+      product_2.valid?.must_equal false
+    end
+
+    it "quantity should be integer" do
+      #products(:blue4).valid?.must_equal false
+      product_2 = Product.new(name: "hat", price: 14.99, quantity: "four")
+      product_2.valid?.must_equal false
+      product_2.errors.messages.must_include :quantity
+    end
   end
 
   describe "relationships" do
