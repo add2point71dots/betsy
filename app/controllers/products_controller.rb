1fb6e998f00b95c23c8c1a50a9cda8139e42129a
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-19 20:35:50 -0700

    Added products#index logic, products#show that shoild be moved to vendors controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 class ProductsController < ApplicationController
   def index
+    if params[:category_id]
+    @products = Product.includes(:categories).where(categories: { id: params[:category_id]})
+    else
+      @products = Product.all
+    end
   end
 
   def show
+    vendor = Vendor.find_by_id(params[:vendor_id])
+    if vendor
+    @products = vendor.products
+    else
+      flash[:warning] = "Invalid vendor id"
+      redirect_back fallback_location: products_path
+    end
   end
 
 
