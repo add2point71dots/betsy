d2c3cce774f2874a985cbddc18afa8e2feb4dd0c
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-20 09:46:06 -0700

    Added products controller and view to resolve conflict

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,39 @@
+class ProductsController < ApplicationController
+  def index
+    if params[:category_id]
+    @products = Product.includes(:categories).where(categories: { id: params[:category_id]})
+    else
+      @products = Product.all
+    end
+  end
+
+  def show
+    @product = Product.find_by(params[:id])
+      if !@product
+        render file: "#{Rails.root}/public/404.html", status: 404
+      end
+  end
+
+
+  def show_by_vendor_id
+    vendor = Vendor.find_by_id(params[:vendor_id])
+    if vendor
+    @products = vendor.products
+    else
+      flash[:warning] = "Invalid vendor id"
+      redirect_back fallback_location: products_path
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
