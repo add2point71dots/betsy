d5829231a367d3db89554c4b980debe9e15f9382
add2point71dots <alcland@gmail.com>
2017-04-19 22:42:09 -0700

    created vendor index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/vendors", to: "vendors#index"
+  get "/vendors/:id", to: "vendors#show", as: "vendor"
 end
