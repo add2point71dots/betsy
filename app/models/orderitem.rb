b3378c4b38511747105f4b6c9ac0007511d5cd7a
Bo Trethewey <botrethewey@gmail.com>
2017-04-22 12:56:08 -0700

    added more tests to orderitem

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,4 +2,10 @@ class Orderitem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, presence: true, numericality: {  only_integer: true, greater_than: 0 }
+
+  # check if quantity is availale
+  def valid_quantity?
+    fufilled_quantity = product.quantity - quantity
+    return fufilled_quantity >= 0
+  end
 end
