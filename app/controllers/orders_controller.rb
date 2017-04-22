17e6f783ab65d2b2049c0dac9efb59ef50cf6aa8
Bo Trethewey <botrethewey@gmail.com>
2017-04-22 16:52:45 -0700

    added order controller action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :find_orderitems, only: [:show]
+
   def index
+    @orders = Order.includes(:orderitems).where(orderitems: { order: params[:id]})
   end
 
-  def show
-  end
+  def show;end
 
   def create
   end
@@ -12,5 +14,12 @@ class OrdersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def find_order
+    @order = Order.find_by_id(params[:id])
+    render_404 if !@order
   end
 end
