8d7dc6f83e9264bc6eb6031b3b998810102f03e6
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-22 11:00:24 -0700

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,43 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "csv"
 
-# CSV.read("support/vendors_seeds.csv", {:headers => true}).each do |data|
-#   args = {
-#           :uid => data[1],
-#           :provider => data[2],
-#           :email => data[3],
-#           :username => data[4]
-#         }
-#   Vendor.create(args)
-# end
-#
-# CSV.read("support/products_seeds.csv", {:headers => true}).each do |data|
-#   args = {
-#           :vendor_id => data[1],
-#           :name => data[2],
-#           :description => data[3],
-#           :photo_url => data[4],
-#           :price => data[5],
-#           :quantity => data[6]
-#         }
-#   Product.create(args)
-# end
-#
-# CSV.read("support/reviews_seeds.csv", {:headers => true}).each do |data|
-#   args = {
-#           :product_id => data[1],
-#           :rating => data[2],
-#           :comment => data[3]
-#         }
-#   Review.create(args)
-# end
-#
-# CSV.read("support/categories_seeds.csv", {:headers => true}).each do |data|
-#   args = {
-#           :label => data[1]
-#         }
-#   Category.create(args)
-# end
+CSV.read("support/vendors_seeds.csv", {:headers => true}).each do |data|
+  args = {
+          :uid => data[1],
+          :provider => data[2],
+          :email => data[3],
+          :username => data[4]
+        }
+  Vendor.create(args)
+end
+
+CSV.read("support/products_seeds.csv", {:headers => true}).each do |data|
+  args = {
+          :vendor_id => data[1],
+          :name => data[2],
+          :description => data[3],
+          :photo_url => data[4],
+          :price => data[5],
+          :quantity => data[6]
+        }
+  Product.create(args)
+end
+
+CSV.read("support/reviews_seeds.csv", {:headers => true}).each do |data|
+  args = {
+          :product_id => data[1],
+          :rating => data[2],
+          :comment => data[3]
+        }
+  Review.create(args)
+end
+
+CSV.read("support/categories_seeds.csv", {:headers => true}).each do |data|
+  args = {
+          :label => data[1]
+        }
+  Category.create(args)
+end
 
 CSV.read("support/orders_seeds.csv", {:headers => true}).each do |data|
   args = {
@@ -73,10 +73,10 @@ CSV.read("support/orderitems_seeds.csv", {:headers => true}).each do |data|
   Orderitem.create(args)
 end
 
-# CSV.read("support/reviews_seeds.csv", {:headers => true}).each do |data|
-#   args = {
-#           :category_id => data[1],
-#           :product_id => data[2],
-#         }
-#   CategoryProduct.create(args)
-# end
+CSV.read("support/reviews_seeds.csv", {:headers => true}).each do |data|
+  args = {
+          :category_id => data[1],
+          :product_id => data[2],
+        }
+  CategoryProduct.create(args)
+end
