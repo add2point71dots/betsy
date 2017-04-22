7ce1c3e217382981e51dd01f197018c04b42c202
Bo Trethewey <botrethewey@gmail.com>
2017-04-22 16:16:41 -0700

    setup restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  
-  get 'orders/index'
-
-  get 'orders/show'
-
-  get 'orders/create'
-
-  get 'orders/edit'
-
-  get 'orders/update'
 
   root to: 'products#index'
 
   get "/vendors", to: "vendors#index"
   get "/vendors/:id", to: "vendors#show", as: "vendor"
+
+  resources :orders
 
   resources :categories do
     resources :products, only: [:index]
