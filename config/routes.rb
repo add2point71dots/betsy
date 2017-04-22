9afa79b004a3893303dc90d46913738fbea46b28
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-22 14:02:50 -0700

    Changed nested category route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'products#index'
 
+  resources :categories do
+    resources :products, only: [:index]
+  end
+
   resources :products
-  get '/products/', to: 'products#index'
 
 
 
