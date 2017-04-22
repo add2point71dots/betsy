84bac05280e3fd4c8f35796dab0ead345a553857
add2point71dots <alcland@gmail.com>
2017-04-22 15:14:24 -0700

    preliminary vendor controller tests

--- test/controllers/vendors_controller_test.rb
+++ test/controllers/vendors_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe VendorsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index" do
+    get vendors_path
+    must_respond_with :success
+  end
+
+  it "should show one vendor" do
+    get vendor_path(vendors(:one))
+    must_respond_with :success
+  end
+
+  it "should show a 404 when vendor not found" do
+    get vendor_path(1)
+    must_respond_with :missing
+  end
 end
