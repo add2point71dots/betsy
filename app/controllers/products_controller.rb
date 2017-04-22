0f0b24b8cd36a57ac7fd62ab68eced20df50e698
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-22 08:19:12 -0700

    Added full crud methods to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+
+  before_action :find_product, only: [:show, :edit, :update]
+
   def index
     if params[:category_id]
     @products = Product.includes(:categories).where(categories: { id: params[:category_id]})
@@ -8,10 +11,41 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find_by_id(params[:id])
       if !@product
         render_404
       end
   end
+
+  def create
+      @product = Product.create(product_params)
+      if @product.save
+        flash[:success] = "New product added"
+        redirect_to products_path
+      else
+        flash.now[:error] = "Failed to add product"
+        render "new"
+      end
+    end
+
+    def edit; end
+
+    def update
+      @product.update(product_params)
+    end
+
+    def destroy
+      Product.destroy(params[:id])
+      redirect_to products_path
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit( :vendor_id, :name, :quantity, :price, :description, :photo_url )
+    end
+
+    def find_product
+    @product = Product.find_by_id(params[:id])
+
 
 end
