57393461c172c17b9b19a45d5a0c84083bbbb688
Bo Trethewey <botrethewey@gmail.com>
2017-04-23 23:30:18 -0700

    add validation method to the orderitem

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -1,14 +1,18 @@
 class Orderitem < ApplicationRecord
   belongs_to :order
-
   belongs_to :product
-  
+  validates :quantity, presence: true, valid_quantity: true, numericality: {  only_integer: true, greater_than: 0 }
+  validates :valid_quantity, on: :create
+  validates :valid_quantity, on: :update
 
-  validates :quantity, presence: true, numericality: {  only_integer: true, greater_than: 0 }
 
-  # check if quantity is availale
-  def valid_quantity?
-    fufilled_quantity = product.quantity - quantity
-    return fufilled_quantity >= 0
+  # checks the availability in the product inventory
+  def valid_quantity
+    unfufilled_quantity = quantity - product.quantity
+    if product.quantity == 0
+      errors.add(:quantity, "This item is sold out")
+    elsif unfufilled_quantity > 0
+      errors.add(:quantity, "Quantity too large: only #{product.quantity} left in stock!")
+    end
   end
 end
