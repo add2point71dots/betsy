5fc901ec882e9b9fa7707f82ffa2bb1d8784e99c
Bo Trethewey <botrethewey@gmail.com>
2017-04-25 09:52:52 -0700

    update create action for Orderitem controller and added custom method in model

--- app/controllers/orderitems_controller.rb
+++ app/controllers/orderitems_controller.rb
@@ -1,7 +1,15 @@
 class OrderitemsController < ApplicationController
-  before_action :current_cart
+  before_action :current_cart, only: [:create, :destroy]
 
   def create
+    @cart.add_to_cart(params)
+    if @cart.save
+      flash[:success] = "#{new_orderitem.product.name} (Qt: #{new_orderitem.quantity}) has been added to your cart"
+      redirect_to cart_path
+    else
+      flash[:error] = "Oops! Something went wrong and we couldn't add this item to your cart"
+      redirect_to product_path(new_orderitem.product_id)
+    end
   end
 
   def update
