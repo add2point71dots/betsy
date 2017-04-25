3fe13d8f49d8a806eaa81ab489ba42a38ad29faf
add2point71dots <alcland@gmail.com>
2017-04-24 20:38:22 -0700

    creates new vendor on first login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
+    vendor = Vendor.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+
+    if vendor.nil?
+      vendor = Vendor.create_from_google(auth_hash)
+      if vendor.nil?
+        flash[:error] = "Could not log in."
+        redirect_to root_path
+      end
+    end
+
+    session[:vendor_id] = vendor.id
+    flash[:success] = "Logged in successfully!"
+    redirect_to root_path
   end
 end
