8cb80e3b819ac5866c595cf24cefd9946e18bdbd
add2point71dots <alcland@gmail.com>
2017-04-25 14:23:45 -0700

    require login before products new and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-
+  before_action :require_login, only: [:new, :create]
   before_action :find_product, only: [:show, :edit, :update]
 
   def index
