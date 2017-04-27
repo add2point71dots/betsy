41c7b51a07627ce0e0bcf8f04aa04b7cac48942c
Bo Trethewey <botrethewey@gmail.com>
2017-04-27 09:45:21 -0700

    1) added status columns to orderitem yml file 2) fixed spelling in orderitem status 3) deleted unused private method in orderitem controller

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,5 +2,5 @@ class Orderitem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, presence: true, numericality: {  only_integer: true, greater_than: 0 }
-  validates_inclusion_of :status, :in => ['Pending', 'Paid', 'Shipped', 'Cancelled'], :allow_nil => true
+  validates_inclusion_of :status, :in => ['Pending', 'Paid', 'Shipped', 'Canceled'], :allow_nil => true
 end
