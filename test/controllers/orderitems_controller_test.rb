0912b813966242456292c0a8fbd83c314c65442d
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-27 18:30:07 -0700

    More Orderitems testing

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -8,20 +8,36 @@ describe OrderitemsController do
     }.must_change 'Orderitem.find(orderitems(:one).id).quantity', 1
   end
 
-  # it "Should not add to cart if the quantity > available stock" do
-  #   proc {
-  #     patch increase_path(orderitems(:one).id), params: { orderitem: {order_id: orders(:one).id, product_id: products(:one).id , quantity: '3', status: "Pending"}
-  #     }
-  #   }.must_change 'Orderitem.find(orderitems(:one).id).quantity', 3
-  #
-  #   flash[:error].must_equal "Oops. Don't be greedy."
-  #   must_redirect_to cart_path
-  # end
+  it "Should not add to cart if the quantity > available stock" do
+    proc {
+      9.times do |i|
+        patch increase_path(orderitems(:one).id), params: { orderitem: {order_id: orders(:one).id, product_id: products(:one).id , quantity: '3', status: "Pending"}
+      }
+    end
 
+  }.must_change 'Orderitem.find(orderitems(:one).id).quantity', 8
 
+    flash[:error].must_equal "Oops. Don't be greedy."
+    must_redirect_to cart_path
+  end
+  it "should be able to cancel" do
 
+    patch cancel_path(orderitems(:one).id)
+     Orderitem.find(orderitems(:one).id).status.must_equal "Cancelled"
+  end
 
+  it "should be able to ship" do
+    patch ship_path(orderitems(:one).id)
+    Orderitem.find(orderitems(:one).id).status.must_equal "Shipped"
+    must_redirect_to fulfillment_path(orderitems(:one).product.vendor_id)
 
+  end
+
+  it "should be able to destroy" do
+    delete orderitem_path(orderitems(:one).id)
+    must_redirect_to cart_path
+
+  end
 
 
 end
